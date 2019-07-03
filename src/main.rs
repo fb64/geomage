51e5761e2cbeb426b887d4d410ab1083bfa38f8f
Florian Bernard <florian.bernard64@gmail.com>
2019-07-03 09:23:29 +0200

    init App with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate clap;
+use clap::{App};
 fn main() {
-    println!("Hello, world!");
+    let yml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yml).get_matches();
+
+    println!("TODO develop app !")
 }
