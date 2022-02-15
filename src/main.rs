5913d58a12a79568edce7bf7948d48d1bb02e2aa
fb64 <florian.bernard64@gmail.com>
2022-02-15 22:00:52 +0100

    Update libs and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-#[macro_use]
-extern crate clap;
-use clap::{App};
+use clap::{arg, App};
+use std::fs::File;
+use std::io::Read;
+use geojson::GeoJson;
+
 fn main() {
-    let yml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yml).get_matches();
+    let matches = App::new("GeoMage")
+        .version("0.1.0")
+        .author("Florian B. <florian@flob.fr>")
+        .about(" Convert geojson to image")
+        .arg(
+            arg!(
+                -i --inputs <INPUT_FILES> "Sets the input geojson files to use"
+            ).required(true))
+        .arg(
+            arg!(
+                -o --output <OUTPUT_FILE> "Sets the input geojson files to use"
+            ).required(true).takes_value(true).required(true))
+        .arg(
+            arg!(
+                -w --width <WIDTH> "Sets the input geojson files to use"
+            ).takes_value(true).default_value("1024").required(false))
+        .get_matches();
 
     println!("TODO develop app !")
 }
